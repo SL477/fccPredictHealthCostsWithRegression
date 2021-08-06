ac741763a9b434413016029bb063596c60d97000
Tom Fishwick <fishwickt@gmail.com>
2021-08-06 20:01:40 +0100

    Update app.py
    started getting data from the tflite file

--- app.py
+++ app.py
@@ -36,6 +36,14 @@ st.write(evaluationStr)
 # Plot the predictions
 test_predictions = model.predict(test_dataset).flatten()
 
+# Get from interpreter
+interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 fig, ax = plt.subplots()
 a = plt.axes(aspect='equal')
 plt.scatter(test_labels, test_predictions)
@@ -111,3 +119,47 @@ df['northeast'] = df['northeast'].astype(np.int32)
 st.write("## Predicted Health Costs")
 #st.write(df.head())
 st.write("Your predicted health costs are ${:,.2f}.".format(model.predict(df)[0][0]))
+
+# test interpreted model on input data
+st.write(input_details[0])
+st.write('shape')
+st.write(input_details[0]['shape'])
+input_data = np.array(
+    [
+        float(age),
+        float(bmi),
+        float(children),
+        float(smoker),
+        float(sex == 'Female'),
+        float(sex == 'Male'),
+        float(region == 'South West'),
+        float(region == 'South East'),
+        float(region == 'North West'),
+        float(region == 'North East')
+    ]
+)
+#input_data = np.array([1.0],[input_data])
+st.write("input_data")
+st.write(input_data)
+st.write("input_data.dtype")
+st.write(input_data.dtype)
+st.write(df.head())
+st.write("interpreter.get_input_details()")
+st.write(interpreter.get_input_details())
+
+input_data2 = np.array(np.random.random_sample(input_details[0]['shape']), dtype=np.float64)
+st.write("input data 2")
+st.write(input_data2)
+st.write("got tensor")
+st.write(interpreter.get_tensor(input_details[0]['index']))
+st.write(type(interpreter.get_tensor(input_details[0]['index'])))
+st.write(interpreter.get_tensor(input_details[0]['index']).dtypes)
+st.write([x.dtype for x in interpreter.get_tensor(input_details[0]['index'])[0]])
+#st.write(interpreter.get_tensor(input_details[0]['dense_input']))
+input_data = interpreter.get_tensor(input_details[0]['index'])
+input_data[0][0] = float(age) * 1.0000
+st.write(input_data)
+interpreter.set_tensor(input_details[0]['index'], df)
+interpreter.invoke()
+
+#https://www.tensorflow.org/lite/guide/inference
